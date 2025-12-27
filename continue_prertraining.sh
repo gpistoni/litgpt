f41793f3c0c7eeabdcf42729c5c72b7a9d8e5879
Giuliano Pistoni <giuliano.pistoni@systemceramics.com>
2025-12-27 12:41:19 +0100

    ct

--- continue_prertraining.sh
+++ continue_prertraining.sh
@@ -5,8 +5,10 @@ curl https://www.gutenberg.org/cache/epub/1513/pg1513.txt --output custom_texts/
 curl https://www.gutenberg.org/cache/epub/84/pg84.txt --output custom_texts/FRANKESTEIN.txt
 curl https://www.gutenberg.org/cache/epub/2600/pg2600.txt --output custom_texts/WAR_AND_PEACE.txt
 
-.venv/bin/pip3 install transformer-engine[core]
+# dep
+.venv/bin/pip3 install transformer-engine[core] nvidia-cudnn-cu12 Tokenizer
 
+# model
 .venv/bin/litgpt download microsoft/phi-2
 
 .venv/bin/litgpt pretrain microsoft/phi-2 \
